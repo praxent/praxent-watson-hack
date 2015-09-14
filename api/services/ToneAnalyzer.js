69b40d26ee1d67202ce06e60f5d36659865c844f
Matt Meyer <matt.meyer@praxent.com>
2015-09-14 11:53:09 -0500

    removing locals.js

--- api/services/ToneAnalyzer.js
+++ api/services/ToneAnalyzer.js
@@ -1,7 +1,16 @@
 var SMILEY = ":-)";
+var watson = require('watson-developer-cloud');
+var authUrl = "https://gateway.watsonplatform.net/tone-analyzer-experimental/api";
+var authorization = watson.authorization(sails.config.watson);
+
+function authCallback(err, token) {
+  console.log("err: " + err);
+  console.log("token: " + token);
+}
 
 module.exports = {
   analyzeTone: function(text) {
+    authorization.getToken({url:authUrl}, authCallback);
     return SMILEY;
   }
 };
