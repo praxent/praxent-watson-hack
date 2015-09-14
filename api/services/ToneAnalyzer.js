c91a4b841c26528a6472c2f7c9908276457b38b8
Matt Meyer <matt.meyer@praxent.com>
2015-09-14 15:54:31 -0500

    Minor cleanup of ToneAnalyzer.js

--- api/services/ToneAnalyzer.js
+++ api/services/ToneAnalyzer.js
@@ -31,21 +31,18 @@ function getToneFromJsonResult(jsonResult) {
   var social  = jsonResult["children"][2]["children"];
 
   tone.emotionValues = [
-    { name: null, value : 0 },
     { name: "cheerful", value : emotion[0].normalized_score },
     { name: "negative", value : emotion[1].normalized_score },
     { name: "angry", value : emotion[2].normalized_score }
   ];
 
   tone.writingValues = [
-    { name: null, value : 0 },
     { name: "analytical", value : writing[0].normalized_score },
     { name: "confident", value : writing[1].normalized_score },
     { name: "tentative", value : writing[2].normalized_score }
   ];
 
   tone.socialValues = [
-    { name: null, value : 0 },
     { name: "open", value : social[0].normalized_score },
     { name: "agreeable", value : social[1].normalized_score },
     { name: "conscientious", value : social[2].normalized_score }
@@ -61,9 +58,9 @@ function getToneFromJsonResult(jsonResult) {
     return -element.value;
   });
 
-  tone.emotion = tone.emotionValues[0].name;
-  tone.writing = tone.writingValues[0].name;
-  tone.social = tone.socialValues[0].name;
+  tone.emotion = tone.emotionValues[0].value == 0 ? null : tone.emotionValues[0].name;
+  tone.writing = tone.writingValues[0].value == 0 ? null : tone.writingValues[0].name;
+  tone.social = tone.socialValues[0].value == 0 ? null : tone.socialValues[0].name;
 
   return tone;
 }
