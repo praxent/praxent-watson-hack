0b71d51cf03eb5a78847ef15da3360789d4955e9
Albert Martin <albert@imor.tl>
2015-09-14 11:10:53 -0500

    Added capabilities route for HipChat integration.

---
+++ api/controllers/HipChatController.js
@@ -0,0 +1,14 @@
+/**
+ * HipChatController
+ *
+ * @description :: Server-side logic for managing HipChat integration.
+ * @help        :: See http://sailsjs.org/#!/documentation/concepts/Controllers
+ */
+
+module.exports = {
+
+  capabilities: function(req, res) {
+    res.json(sails.config.hipchat.capabilities);
+  }
+
+};
---
+++ config/hipchat.js
@@ -0,0 +1,7 @@
+module.exports.hipchat = {
+  capabilities: {
+    name: "Praxent HipChat Bot",
+    description: "HipChat bot for the Praxent team.",
+    key: "com.praxent.bot"
+  }
+}
