2f0f40208911f4321be82558235d6698f6f4baad
Bronius Motekaitis <bronius.motekaitis@gmail.com>
2015-09-14 16:08:45 -0500

    Merges in Matt's ToneAnalyzer changes for distilled tones

--- api/controllers/WatsonController.js
+++ api/controllers/WatsonController.js
@@ -11,18 +11,55 @@ module.exports = {
 
     if (!req.body) return res.forbidden();
 
-    var context = HipChat.commandContext(req.body);
+    var context = HipChat.commandContext(req.body),
+    		message,
+    		color;
+
     HipChat.history(context, function(err, messageText) {
 
-      // @todo: Replace with call to Watson analyzing 'messageText'!
       sails.log.info(messageText);
-      var mood = '¯\\_(ツ)_/¯';
 
-      res.json({
-        color: 'green',
-        message: mood,
-        notify: false,
-        message_format: 'text'
+      ToneAnalyzer.analyzeTone(messageText, function(err, tone) {
+      	if (err) {
+      		sails.log.info("watson error: ", err);
+      	} else {
+      		// Expected response is an object with three categories and one string value attributed to each:
+      		// tone.emotion = {'cheerful', 'negative', 'angry'}
+      		// tone.writing = {'analytical', 'confident', 'tentative'}
+					// tone.social = {'open', 'agreeable', 'conscientious'}
+      		sails.log.info("watson: ", tone);
+      		var emotion = 'angry'; // tone.emotion
+
+      		var results = {
+      			cheerful: { color: 'green', message: 'saf' }
+      		};
+
+      		res.json(results[emotion]);
+
+      		switch (emotion) {
+      			case 'cheerful':
+			        color = 'green';
+			        message = '(awesome)';
+      				break;
+      			case 'negative':
+			        color = 'black';
+			        message = '(wat)';
+      				break;
+      			case 'angry':
+			        color = 'red';
+			        message = '(tableflip)';
+      				break;
+      			default: '';
+      		}
+
+		      res.json({
+		        color: color,
+		        message: message,
+		        notify: false,
+		        message_format: 'text'
+		      });
+
+      	}
       });
 
     });
---
+++ api/services/ReplyBuilder.js
@@ -0,0 +1,50 @@
+module.exports = {
+  buildReply: function(text, callback) {
+    ToneAnalyzer.analyzeTone(text, function(err, tone) {
+    	if (err) {
+    		sails.log.info("watson error: ", err);
+    	} else {
+    		// Expected response is an object with three categories and one string value attributed to each:
+    		// tone.emotion = {'cheerful', 'negative', 'angry'}
+    		// tone.writing = {'analytical', 'confident', 'tentative'}
+				// tone.social = {'open', 'agreeable', 'conscientious'}
+    		sails.log.info("watson: ", tone);
+    		var message,
+    				color;
+
+    		var emotion = 'angry'; // tone.emotion
+
+    		// var results = {
+    		// 	cheerful: { color: 'green', message: 'saf' }
+    		// };
+
+    		// res.json(results[emotion]);
+
+    		switch (emotion) {
+    			case 'cheerful':
+		        color = 'green';
+		        message = '(awesome)';
+    				break;
+    			case 'negative':
+		        color = 'black';
+		        message = '(wat)';
+    				break;
+    			case 'angry':
+		        color = 'red';
+		        message = '(tableflip)';
+    				break;
+    			default: '';
+    		}
+
+	      callback({
+	        color: color,
+	        message: message,
+	        notify: false,
+	        message_format: 'text'
+	      });
+
+    	}
+    });
+
+  }
+};
