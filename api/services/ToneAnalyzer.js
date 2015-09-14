d5cab6abf359fd45b5686363e8196e3a8321196c
Matt Meyer <matt.meyer@praxent.com>
2015-09-14 15:41:04 -0500

    Fleshing out services/ToneAnalyzer.js to give back a filled out object representing the relevant tones

--- api/services/ToneAnalyzer.js
+++ api/services/ToneAnalyzer.js
@@ -4,9 +4,66 @@ var tone_analyzer = watson.tone_analyzer({
   password:sails.config.watson.password,
   version:'v1'
 });
+var CUTOFF = 0.5;
+var emotions = [
+  "cheerful",
+  "negative",
+  "angry"
+];
 
 module.exports = {
   analyzeTone: function(text, callback) {
-    tone_analyzer.tone({text:"test"}, callback);
+    tone_analyzer.tone({text:text}, function(error, result) {
+      if(error) {
+        callback(error);
+      } else {
+        callback(null, getToneFromJsonResult(result));
+      }
+    });
   }
 };
+
+function getToneFromJsonResult(jsonResult) {
+  var tone = {};
+
+  var emotion = jsonResult["children"][0]["children"];
+  var writing = jsonResult["children"][1]["children"];
+  var social  = jsonResult["children"][2]["children"];
+
+  tone.emotionValues = [
+    { name: null, value : 0 },
+    { name: "cheerful", value : emotion[0].normalized_score },
+    { name: "negative", value : emotion[1].normalized_score },
+    { name: "angry", value : emotion[2].normalized_score }
+  ];
+
+  tone.writingValues = [
+    { name: null, value : 0 },
+    { name: "analytical", value : writing[0].normalized_score },
+    { name: "confident", value : writing[1].normalized_score },
+    { name: "tentative", value : writing[2].normalized_score }
+  ];
+
+  tone.socialValues = [
+    { name: null, value : 0 },
+    { name: "open", value : social[0].normalized_score },
+    { name: "agreeable", value : social[1].normalized_score },
+    { name: "conscientious", value : social[2].normalized_score }
+  ];
+
+  tone.emotionValues = _.sortBy(tone.emotionValues, function(element) {
+    return -element.value;
+  });
+  tone.writingValues = _.sortBy(tone.writingValues, function(element) {
+    return -element.value;
+  });
+  tone.socialValues = _.sortBy(tone.socialValues, function(element) {
+    return -element.value;
+  });
+
+  tone.emotion = tone.emotionValues[0].name;
+  tone.writing = tone.writingValues[0].name;
+  tone.social = tone.socialValues[0].name;
+
+  return tone;
+}
