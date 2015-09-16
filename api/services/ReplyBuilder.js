23972bc168bba38fe3771064df3c6c74ffe63e0e
Albert Martin <albert@imor.tl>
2015-09-16 12:32:41 -0500

    Fixed color mapping to emotional tones.

--- api/services/ReplyBuilder.js
+++ api/services/ReplyBuilder.js
@@ -1,45 +1,40 @@
 module.exports = {
   buildReply: function(text, callback) {
     ToneAnalyzer.analyzeTone(text, function(err, tone) {
-    	if (err) {
-    		sails.log.info("watson error: ", err);
-    	} else {
-    		// Expected response is an object with three categories and one string value attributed to each:
-    		// tone.emotion = {'cheerful', 'negative', 'angry'}
-    		// tone.writing = {'analytical', 'confident', 'tentative'}
-    		// tone.social = {'open', 'agreeable', 'conscientious'}
-    		sails.log.info("watson: ", tone);
-    		var message,
-    				color;
+      if (err)
+        return sails.log.error(err);
 
-    		var emotion = tone.emotion,
-            writing = tone.writing;
+      // Expected response is an object with three categories and one string value attributed to each:
+      // tone.emotion = {'cheerful', 'negative', 'angry'}
+      // tone.writing = {'analytical', 'confident', 'tentative'}
+      // tone.social = {'open', 'agreeable', 'conscientious'}
+      sails.log.info('Watson returned tone data:', tone);
 
-    		var replies = {
-          cheerful: { color: 'green', message: '(awesome)' },
-          negative: { color: 'black', message: '(wat)' },
-    		  angry: { color: 'black', message: '(tableflip)' },
-          default: { color: 'grey', message: '¯\\_(ツ)_/¯' }
-    		};
+      var emotion = tone.emotion,
+          writing = tone.writing;
 
-        var writing_emoticons = {
-          analytical: '(indeed)',
-          confident: '(winning)',
-          tentative: '(disappear)'
-        };
+      var replies = {
+        cheerful: { color: 'green', message: '(awesome)' },
+        negative: { color: 'yellow', message: '(wat)' },
+        angry: { color: 'red', message: '(tableflip)' },
+        default: { color: 'gray', message: '¯\\_(ツ)_/¯' }
+      };
 
-        var reply = replies[emotion] || replies['default'];
+      var writing_emoticons = {
+        analytical: '(indeed)',
+        confident: '(winning)',
+        tentative: '(disappear)'
+      };
 
-        reply.message += writing_emoticons[writing] || writing_emoticons['default'];
+      var reply = replies[emotion] || replies['default'];
+      reply.message += writing_emoticons[writing] || writing_emoticons['default'];
 
-	      callback({
-	        color: reply.color,
-	        message: reply.message,
-	        notify: false,
-	        message_format: 'text'
-	      });
-
-    	}
+      callback({
+        color: reply.color,
+        message: reply.message,
+        notify: false,
+        message_format: 'text'
+      });
     });
 
   }
